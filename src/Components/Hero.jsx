8a690caeb84c4a77d6642174d702fe60ca13ea22
mohdhadi01 <hadi.88923@gmail.com>
2024-02-07 19:51:27 +0530

    #removed framer and applied hardcore animation

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,6 @@ import "./Hero.css"
 import profileImg from "../Assets/edited_dp-ast6yqVUK-transformed.png"
 import Arrow from "../Assets/arrow.png"
 import Twitter from "../Assets/twitter (2).png"
-import { motion } from 'framer-motion';
 
 function Hero() {
 
@@ -14,89 +13,37 @@ function Hero() {
         </div>
         <div name="home" className='Hero'>
             <div className="Desc">
-                <motion.h3
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 0.2,
-                        duration: 1.5
-                    }}
-                    className='gradientText helloText font-semibold text-lg my-1'>Hello There !</motion.h3>
-                <motion.h2
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 0.6,
-                        duration: 1.5
-                    }}
-                    className='Name text-3xl '>I'm Mohd Hadi</motion.h2>
-                <motion.h2
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 0.7,
-                        duration: 1.5
-                    }}
-                    className='gradientText frontendDev text-5xl font-medium'>Frontend Developer</motion.h2>
-                <motion.h2
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 0.9,
-                        duration: 1.5
-                    }}
-                    className='para text-sm'>I like building new things and turn visions into vibrant code as a frontend wizard. I worked on Many projects as a developer using most recent Technologies.</motion.h2>
+                <h3
+                    className='gradientText helloText font-semibold text-lg my-1' style={{ animation: "Text-opacity 1s 0.2s both" }}>Hello There !</h3>
+                <h2
+                    className='Name text-3xl' style={{ animation: "Text-opacity 1.5s 0.4s both" }}>I'm Mohd Hadi</h2>
+                <h2
+                    className='gradientText frontendDev text-5xl font-medium' style={{ animation: "Text-opacity 1.5s 0.6s both" }}>Frontend Developer</h2>
+                <h2
+                    className='para text-sm' style={{ animation: "Text-opacity 2.5s 0.8s ease-in-out both" }}>I like building new things and turn visions into vibrant code as a frontend wizard. I worked on Many projects as a developer using most recent Technologies.</h2>
             </div>
-            <motion.div
-                initial={{ rotate: 180, scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                transition={{
-                    type: "spring",
-                    stiffness: 60,
-                    // damping: 20,
-                    delay: 1.1,
-                    duration: 1.5
-                }}
+            <div
                 className="Image">
-                <img src={profileImg} className='myImage' alt="" />
-            </motion.div>
+                <img loading='lazy' src={profileImg} className='myImage' alt="" />
+            </div>
 
         </div>
         <div className="buttons">
             <a href="https://drive.google.com/file/d/1JD2K5xF108FjhZmcTlFYeoEPk69SN-va/view?usp=drive_link">
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 1.2,
-                        duration: 1.5
-                    }}
-                    className="Resume">Download CV</motion.div></a>
+                <div
+                    className="Resume" style={{ animation: "Text-opacity 2.5s 1.2s ease-in-out both" }}>Download CV</div></a>
 
             <a href="mailto: hadi.88923@gmail.com">
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                        delay: 1.5,
-                        duration: 1.5
-                    }}
-                    className="HireMe">Hire Me</motion.div></a>
+                <div className="HireMe" style={{ animation: "Text-opacity 2.5s 1.5s ease-in-out both" }}>Hire Me</div></a>
             <img className='Arrow' src={Arrow} alt="" />
         </div>
-        <motion.div
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{
-                delay: 1.2,
-                duration: 1.5
-            }}
+        <div
             className="Socials mx-8 my-10 flex gap-6 justify-center">
-            <a href="mailto: hadi.88923@gmail.com"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 24 24" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"></path></svg></a>
-            <a href="https://github.com/mohdhadi01"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 1024 1024" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg></a>
-            <a href="https://www.linkedin.com/in/mohd-hadi-5a4638226/"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 448 512" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a>
-            <a href="https://twitter.com/mohdhadi01"><img src={Twitter} className='SocialIcons' alt="" /></a>
-        </motion.div>
+            <a style={{ animation: "scale-zoom 1.5s 1s ease-in-out both" }} href="mailto: hadi.88923@gmail.com"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 24 24" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"></path></svg></a>
+            <a style={{ animation: "scale-zoom 1.5s 1s ease-in-out both" }} href="https://github.com/mohdhadi01"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 1024 1024" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg></a>
+            <a style={{ animation: "scale-zoom 1.5s 1s ease-in-out both" }} href="https://www.linkedin.com/in/mohd-hadi-5a4638226/"><svg stroke="#ff7a2c" fill="#ce6629f5" stroke-width="0" viewBox="0 0 448 512" class="SocialIcons text-secondary hover:text-secondary/50  text-2xl md:text-3xl " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a>
+            <a style={{ animation: "scale-zoom 1.5s 1s ease-in-out both" }} href="https://twitter.com/mohdhadi01"><img src={Twitter} className='SocialIcons' alt="" /></a>
+        </div>
 
     </>)
 }
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -10,7 +10,6 @@ import Python from "../Assets/Skills/icons8-python-96.png"
 import ReactIcon from "../Assets/Skills/icons8-react-a-javascript-library-for-building-user-interfaces-96.png"
 import Redux from "../Assets/Skills/icons8-redux-an-open-source-javascript-library-for-managing-application-state-96.png"
 import Tailwind from "../Assets/Skills/icons8-tailwindcss-96.png"
-import {motion} from "framer-motion"
 
 function Skills() {
     return (<>
@@ -19,43 +18,27 @@ function Skills() {
                 <div class="main-gradient left-0 top-80"></div>
                 <div class="main-gradient right-0 top-5"></div>
             </div>
-            <motion.h1
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{
-              delay:0.5,
-              duration:0.3
-            }}
-            
-            className='gradientText SkillHeading text-4xl text-center font-medium my-unit-4xl'>Skills</motion.h1>
+            <h1
+          
+            className='gradientText SkillHeading text-4xl text-center font-medium my-unit-4xl'>Skills</h1>
             <div
             className="skillContainer">
-                <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{
-                delay:0.5,
-                duration:0.8
-              }} className="Line1 flex justify-around">
+                <div
+               className="Line1 flex justify-around">
                     <div className="SkillIcons flex flex-col"><img className='Icon' src={HtmlIcon} alt="" />HTML</div>
                     <div className="SkillIcons flex flex-col"><img className='Icon' src={CssIcon} alt="" />CSS</div>
                     <div className="SkillIcons flex flex-col"> <img className='Icon' src={ReactIcon} alt="" />React</div>
                     <div className="SkillIcons MidLine-1a flex flex-col"> <img className='Icon' src={Redux} alt="" />Redux</div>
                     <div className="SkillIcons MidLine-1b flex flex-col"> <img className='Icon' src={JSIcon} alt="" />Javascript</div>
-                </motion.div>
-                <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{
-                delay:0.9,
-                duration:0.8
-              }} className="Line2 flex justify-center gap-8 my-11">
+                </div>
+                <div
+               className="Line2 flex justify-center gap-8 my-11">
                     <div className="SkillIcons flex flex-col"><img className='Icon' src={Tailwind} alt="" />Tailwind</div>
                     <div className="SkillIcons flex flex-col"><img className='Icon' src={Github} alt="" />Github</div>
                     <div className="SkillIcons flex flex-col"><img className='Icon' src={photoshop} alt="" />Photoshop</div>
                     <div className="SkillIcons MidLine-1a flex flex-col"><img className='Icon' src={Java} alt="" />Java</div>
                     <div className="SkillIcons MidLine-1b flex flex-col"><img className='Icon' src={Python} alt="" />Python</div>
-                </motion.div>
+                </div>
 
             </div>
 

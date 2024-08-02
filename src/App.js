338286315ebb2fee36798c7ea5eb8f8e919e7188
mohdhadi01 <hadi.88923@gmail.com>
2024-08-03 01:43:46 +0530

    added vercel analytics'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Mainpage from './Pages/Mainpage';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <Mainpage/>
+      <Analytics/>
     </div>
   );
 }
